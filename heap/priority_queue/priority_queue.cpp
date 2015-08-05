1667d5ec7c1c4066afaa8d194334850dbf285ad1
guker <huqunwei2607@gmail.com>
2015-08-05 13:50:18 +0800

    max heap using STL

---
+++ heap/priority_queue/priority_queue.cpp
@@ -0,0 +1,41 @@
+/*++++++++++++++++++++++++++++++++++++++++++++++
+*                 说明
+*     使用STL的优先队列实现最大堆 topK
+*     时间复杂度O(N*logN)
+*          
+*     @date  2015/08/05
+*
+*
++++++++++++++++++++++++++++++++++++++++++++++++*/
+#include <iostream>
+#include<queue>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define HEAP_SIZE  2   ///堆的大小
+
+int main()
+{
+	int num[] = {5,-6,1,3,4,9,0,-1,-2};
+    /// 优先队列
+	std::priority_queue<int> q;
+	/// 数组大小
+	int len = sizeof(num)/(sizeof(int));
+	for(int i =0; i<len; ++i)
+	{
+		if(q.size() <= HEAP_SIZE)
+			q.push(num[i]);
+		else
+		{
+			///cout<<q.top()<<endl;
+			q.pop();
+			q.push(num[i]);
+		}
+	}
+	return 0;
+	
+
+
+}
